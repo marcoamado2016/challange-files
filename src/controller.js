4592fafd385b9f33f6bae1f6e8901ee8b3f519a0
marco amado <marcos242016@gmail.com>
2025-06-25 09:33:25 -0300

    subir archivo desde postman para procesar el contenido

--- src/controller.js
+++ src/controller.js
@@ -1,11 +1,15 @@
 const Records = require('./records.model');
-
+const fs = require('fs')
 const upload = async (req, res) => {
-    const {file} = req;
+    const { file } = req;
+    try {
+        const content = fs.readFileSync(file.path, 'utf-8');
+        console.log("contenido ", content)
+        return res.status(200).json({ message: 'some response', content });
+    } catch (error) {
+        return res.status(500).json(error)
+    }
 
-    /* Acá va tu código! Recordá que podés acceder al archivo desde la constante file */
-
-    return res.status(200).json({ message: 'some response' });
 };
 
 const list = async (_, res) => {
@@ -14,7 +18,7 @@ const list = async (_, res) => {
             .find({})
             .limit(10)
             .lean();
-        
+
         return res.status(200).json(data);
     } catch (err) {
         return res.status(500).json(err);
